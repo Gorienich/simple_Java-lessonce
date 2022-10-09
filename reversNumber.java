55f4c096c3ddad0f8b0026109ed791cc1ceda694
Gorienich <107270919+Gorienich@users.noreply.github.com>
2022-10-09 21:31:04 +0300

    revers number
    :)

---
+++ reversNumber.java
@@ -0,0 +1,18 @@
+package simpleJava;
+import java.util.Scanner;
+public class reversNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner in = new Scanner(System.in);
+System.out.println("enter a number to revers");
+int num = in.nextInt();
+int digit = 0;
+while(num != 0) {
+	digit = num%10;
+	System.out.println(digit);
+	num/=10;
+}
+	}
+Scanner close;
+}
