09d88c5f402ed51aa0f12be7352875ddbc55182c
Gorienich <107270919+Gorienich@users.noreply.github.com>
2022-10-21 17:43:12 +0300

    interface function to all geo

---
+++ Simple classes/geometriya/interface for all.java
@@ -0,0 +1,7 @@
+package R28q2;
+
+public interface shape {
+	public double area (double length,double width);    // num = length num2 = width
+	public double peremiter (double length,double width);  
+	public void print();
+}
