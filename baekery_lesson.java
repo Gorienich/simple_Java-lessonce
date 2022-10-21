d0fda2cda2b696c028dde24537345902a0bc891a
Gorienich <107270919+Gorienich@users.noreply.github.com>
2022-10-21 17:01:55 +0300

    backery lesson
    ...

--- baekery_lesson.java
+++ baekery_lesson.java
@@ -1,38 +1,61 @@
-package simpleJava;
-import java.util.Scanner;
-public class baekery_lesson {
 /*
- * this lesson to count marketing of bakery
+ * algorithm:
+ * 
+ * 1. define a variable double: sandwiches = 750;
+ * 2. define a variable double: amount = 0;
+ * 3. define a variable double: double money = 0;
+ * 4. define a variable int: buys = 0;
+ * 5. until sandwiches>0;
+ * 5.1 output: "how many sandwiches you want to buy? "
+ * 5.2 input: amount from user;
+ * 5.3 buys +1;
+ * 5.4 condition: amount>sandwiches;
+ * 5.4.1 output: how many sandwiches user can buy;
+ * 5.4.2 define amount to sandwiches;
+ * 5.5 condition: amount<10;
+ * 5.5.1 define how many sandwiches left after buying
+ * 5.5.2 define how money user have to pay
+ * 5.5.3 output: display to user how money user have to pay
+ * 5.6 opposite condition: define how many sandwiches left after buying
+ * 5.6.1 define how money user have to pay
+ * 5.6.2 output: display to user how money user have to pay
+ * 6. output: display to user "no sandwiches left"
+ * 7. output: total money earned,   total purchases;
+
  */
+import java.util.Scanner;
+public class Q1N7{
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Scanner in = new Scanner(System.in);
-int sandwiches = 750;
-double amount =0.0;
-double money = 0.0;
-int buys =0;
-while(sandwiches > 0) {
-	System.out.println("how many sandwiches you want to buy?");   // not 0
-	amount = in.nextInt();
-	buys++;
-	if(amount > sandwiches) {
-		System.out.println("there is only: " + sandwiches + " you can buy.. ");
-		amount = sandwiches;  // to sell all we have
-	}
-	if(amount <10) {
-		sandwiches -= amount;
-		System.out.println("you have to pay: " + (amount*2));
-		money += amount*2; // without super praise
-	}
-	else {
-		sandwiches -= amount;
-		System.out.println("you have to pay: " + (amount*1.5));
-		money += amount * 1.5; // with super praise
-	}
-	
-}
-System.out.println("run out of sandwiches...");
-System.out.println("total money: " + money + "total buys: " + buys);
+		 Scanner input = new Scanner(System.in);
+		    double  sandwiches = 750;    // define total sandwiches
+		    double amount = 0;           // define amount = 0
+		    double money = 0;             // define money = 0
+		    int buys = 0;                 // define buys = 0
+		   while(sandwiches>0)            // program run until sandwiches>0
+		    { 
+		      System.out.println( "how many sandwiches you want to buy? "); // question to user 
+		       amount = input.nextInt();                                    // input from user 
+		       buys++;                                                      // buys +1
+		       if(amount>sandwiches){                                       // condition: amount>sandwiches
+		          System.out.println( "the is only  " + sandwiches + " sandwiches left"); // output to user how many sandwiches left
+		          amount=sandwiches;                                                      // define user amount to sandwiches
+		       }
+		       if(amount <10)                                                              // condition: amount <10
+		      { sandwiches=sandwiches-amount;                                              // define how many sandwiches left
+		         money+=amount*2;                                                          // define cost of sandwiches     
+		       System.out.println("You need to pay: " + (amount*2));                       // output: display to user cost of sandwiches
+		       }
+		       else                                                                        // opposite condition:
+		       {
+		    	   sandwiches=sandwiches-amount;                                           // define how many sandwiches left after user input
+		           money+=amount*1.5;                                                      // define cost of sandwiches
+		           System.out.println("You need to pay: " + (amount*1.5));                 // output to user: cost of sandwiches
+		       }
+		  }
+		 System.out.println("Run out of sandiches: "  );                                   // output to user: no sandwiches left
+	     System.out.println("total money earned: " + money + "IL;   total purchases: " + buys  );  // output to owner: total money earned:, total purchases
 	}
 
 }
