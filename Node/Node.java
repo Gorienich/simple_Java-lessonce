6d906d3ebb10440d9e4855dc2bf2327a6ad658ee
Gorienich <107270919+Gorienich@users.noreply.github.com>
2023-02-18 20:15:51 +0200

    Create Node.java

---
+++ Node/Node.java
@@ -0,0 +1,32 @@
+public class Node<T> {
+
+	private T value;
+	private Node<T> next;
+	
+	public Node(T value, Node<T> next) 
+	{ 
+		this.value= value; 
+		this.next= next; 
+	} 
+	
+	public Node<T> getNext()        
+	{
+		return this.next;
+	} 
+	
+	public void setNext(Node<T> next) 
+	{
+		this.next=next;
+	} 
+	
+	public T getvalue()              
+	{
+		return this.value;
+	} 
+	
+	public void setvalue(T value)       
+	{
+		this.value=value;
+	}
+	
+}
